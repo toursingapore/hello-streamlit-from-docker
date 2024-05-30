17e9df173eb153e4f95da1ffc8b186279f143a0d
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-05-30 11:33:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,24 @@
 import streamlit as st
 import torch
+import cv2
 from segment_anything import sam_model_registry
 
 def main():
     st.title("Hello Streamlit App")
-    st.write("Welcome to the Hello Streamlit app!")   
-    name = st.text_input("Enter your name:")   
+    st.write("Welcome to the Hello Streamlit app!")
+    name = st.text_input("Enter your name:")
+    
     if st.button("Greet"):
         st.write(f"Hello, {name}! Nice to meet you!")
 
-    import cv2
-    st.write(f"opencv version {cv2.__version__}")
+    st.write(f"opencv version: {cv2.__version__}")
 
-    if torch.cuda.is_available():
-       device = "cuda"
-    else:
-       device = "cpu"
-    st.write(device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    st.write(f"Using device: {device}")
     
     # Load the SAM model
-    sam = sam_model_registry["vit_l"](checkpoint="./sam_images/sam_vit_l_0b3195.pth")    
+    sam = sam_model_registry["vit_l"](checkpoint="./sam_images/sam_vit_l_0b3195.pth")
     sam.to(device=device)
-
 
 if __name__ == "__main__":
     main()
